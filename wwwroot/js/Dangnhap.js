edd311fe1b83492f1467f310683e81a498d91347
DieuuHuyenn <huyendieuu07@gmail.com>
2025-07-10 02:38:11 +0700

    pull

---
+++ wwwroot/js/Dangnhap.js
@@ -0,0 +1,8 @@
+﻿document.getElementById("togglePassword").addEventListener("click", function () {
+    const passwordInput = document.getElementById("password");
+    const type = passwordInput.type === "password" ? "text" : "password";
+    passwordInput.type = type;
+
+    this.classList.toggle("ri-eye-fill");
+    this.classList.toggle("ri-eye-off-fill");
+});
