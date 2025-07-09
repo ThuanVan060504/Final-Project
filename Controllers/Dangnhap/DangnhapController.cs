752df61eb03291db97ac0a453b5264b813ef624b
Joinney <voduyduydlk@gmail.com>
2025-07-09 21:08:04 +0700

    up up

---
+++ Controllers/Dangnhap/DangnhapController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class DangnhapController : Controller
+{
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View("~/Dangnhap/dangnhap.cshtml");
+
+
+    }
+
+    [HttpPost]
+    public IActionResult Index(string username, string password)
+    {
+        if (username == "admin" && password == "123")
+        {
+            TempData["LoginSuccess"] = "Đăng nhập thành công!";
+            return RedirectToAction("Index", "Home");
+        }
+
+        ViewBag.Error = "Tên đăng nhập hoặc mật khẩu không đúng.";
+        return View("~/Dangnhap/dangnhap.cshtml");
+
+
+    }
+}
