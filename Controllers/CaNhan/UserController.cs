a4b9a063f5c6e819718f99661e2e2d03f9108604
ThuanIT <thugoodcat@gmail.com>
2025-07-16 13:58:57 +0700

    up

---
+++ Controllers/CaNhan/UserController.cs
@@ -0,0 +1,33 @@
+﻿using Final_Project.Models.Shop;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Final_Project.Controllers
+{
+    [Authorize] // Chặn chưa login
+    public class UserController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public UserController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Profile()
+        {
+            // Lấy Email của người dùng từ Claims
+            var email = User.FindFirstValue(ClaimTypes.Email);
+
+            // Tìm user theo Email
+            var user = _context.TaiKhoans.FirstOrDefault(u => u.Email == email);
+            if (user == null)
+            {
+                return NotFound("Không tìm thấy thông tin người dùng.");
+            }
+
+            return View(user); // Truyền user qua view
+        }
+    }
+}
