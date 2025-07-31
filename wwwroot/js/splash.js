909c4938e209604a95e88bfed7693940290c59a0
ThuanVan060504 <nhockisolo@gmail.com>
2025-07-31 17:25:00 +0700

    layout mới cho trang web

---
+++ wwwroot/js/splash.js
@@ -0,0 +1,15 @@
+﻿window.addEventListener('load', function () {
+    const splash = document.getElementById('splash-screen');
+
+    setTimeout(() => {
+        // 1. Ẩn splash
+        if (splash) {
+            splash.classList.add('hidden');
+        }
+
+        // 2. Sau đó apply hiệu ứng cho trang chính
+        setTimeout(() => {
+            document.body.classList.add('body-appear');
+        }, 400); // delay nhẹ sau khi splash ẩn (0.4s)
+    }, 1200); // thời gian chờ splash (1.4s)
+});
