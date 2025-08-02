89f684895fc6115a1a53aa686fba65094676538f
Joinney <voduyduydlk@gmail.com>
2025-08-02 11:45:05 +0700

    up view nhapkho

---
+++ Adminboot/Admin/Controllers/NhapKho/NhapKhoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Final_Project.Adminboot.Admin.Controllers.NhapKho
+{
+   
+     [Area("Admin")]
+      public class NhapKhoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("~/Adminboot/Admin/Views/NhapKhoSP/Index.cshtml");
+        }
+
+
+    }
+}
