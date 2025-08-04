76cc9a06d8919660c87cd1db2a65b5dc6cc8e226
ThuanIT <thugoodcat@gmail.com>
2025-08-05 01:10:39 +0700

    TimKiemSP

--- Controllers/Menu/SanPhamController.cs
+++ Controllers/Menu/SanPhamController.cs
@@ -160,6 +160,48 @@ namespace Final_Project.Controllers.Menu
             return RedirectToAction("Details", new { id = SanPhamId });
         }
 
-        
+        public IActionResult Search(string keyword, int page = 1)
+        {
+            int? maTK = HttpContext.Session.GetInt32("MaTK");
+            if (maTK != null)
+            {
+                var taiKhoan = _context.TaiKhoans.FirstOrDefault(t => t.MaTK == maTK);
+                ViewBag.Avatar = taiKhoan?.Avatar;
+                ViewBag.HoTen = taiKhoan?.HoTen;
+            }
+            if (string.IsNullOrWhiteSpace(keyword))
+            {
+                return RedirectToAction("Index");
+            }
+
+            int pageSize = 6;
+
+            var products = _context.SanPhams
+                .Include(p => p.DanhMuc)
+                .Where(p => p.TenSP.Contains(keyword))
+                .OrderBy(p => p.MaSP);
+
+            int totalItems = products.Count();
+            int totalPages = (int)Math.Ceiling((double)totalItems / pageSize);
+
+            var pagedProducts = products
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                .ToList();
+
+            var soldQuantities = _context.ChiTietDonHangs
+                .GroupBy(ct => ct.MaSP)
+                .Select(g => new { MaSP = g.Key, SoLuongDaBan = g.Sum(ct => ct.SoLuong) })
+                .ToDictionary(x => x.MaSP, x => x.SoLuongDaBan);
+
+            ViewBag.SoLuongDaBan = soldQuantities;
+            ViewBag.CurrentPage = page;
+            ViewBag.TotalPages = totalPages;
+            ViewBag.Keyword = keyword;
+
+            return View("Index", pagedProducts);
+        }
+
+
     }
 }
