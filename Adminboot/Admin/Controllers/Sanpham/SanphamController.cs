bd0b94f5258cc0ac5c995ca050050a392efbf69f
Joinney <voduyduydlk@gmail.com>
2025-08-08 18:43:06 +0700

    tạo được sản phẩm ok

--- Adminboot/Admin/Controllers/Sanpham/SanphamController.cs
+++ Adminboot/Admin/Controllers/Sanpham/SanphamController.cs
@@ -1,94 +1,113 @@
 ﻿using Final_Project.Models.Shop;
-using Final_Project.Models.ViewModels;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using System;
 using System.IO;
-using System.Linq;
 using System.Threading.Tasks;
 
-namespace Final_Project.Adminboot.Admin.Controllers.Sanpham
+namespace Final_Project.Areas.Admin.Controllers
 {
     [Area("Admin")]
-    public class SanphamController : Controller
+    public class SanPhamController : Controller
     {
         private readonly AppDbContext _context;
-        private readonly IWebHostEnvironment _hostEnvironment;
 
-        public SanphamController(AppDbContext context, IWebHostEnvironment hostEnvironment)
+        public SanPhamController(AppDbContext context)
         {
             _context = context;
-            _hostEnvironment = hostEnvironment;
         }
+
+        // Danh sách sản phẩm
         public IActionResult Index()
         {
-            var products = _context.SanPhams
-                .Include(p => p.DanhMuc)
-                .Include(p => p.ThuongHieu)
+            var sanPhamList = _context.SanPhams
+                .Include(s => s.DanhMuc)
+                .Include(s => s.ThuongHieu)
                 .ToList();
 
-            return View("~/Adminboot/Admin/Views/Sanpham/Index.cshtml", products);
+            return View("~/Adminboot/Admin/Views/Sanpham/Index.cshtml", sanPhamList);
         }
-        // GET: Admin/Sanpham/Create
+
+        // Form thêm sản phẩm
         public IActionResult Create()
         {
-            ViewBag.DanhMucList = new SelectList(_context.DanhMucs.ToList(), "MaDanhMuc", "TenDanhMuc");
-            ViewBag.ThuongHieuList = new SelectList(_context.ThuongHieus.ToList(), "MaThuongHieu", "TenThuongHieu");
-
+            ViewBag.DanhMucList = new SelectList(_context.DanhMucs, "MaDanhMuc", "TenDanhMuc");
+            ViewBag.ThuongHieuList = new SelectList(_context.ThuongHieus, "MaThuongHieu", "TenThuongHieu");
             return View("~/Adminboot/Admin/Views/Sanpham/Create.cshtml");
         }
 
-        // POST: Admin/Sanpham/Create
+        // Xử lý thêm sản phẩm
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(SanPhamViewModel model)
+        public async Task<IActionResult> Create(
+            string TenSP,
+            string MoTa,
+            decimal DonGia,
+            decimal GiaGoc,
+            int SoLuong,
+            double ChieuRong,
+            double ChieuCao,
+            double ChieuSau,
+            int MaDanhMuc,
+            int MaThuongHieu,
+            IFormFile Anh,
+            string ImageURL)
         {
-            if (ModelState.IsValid)
+            if (string.IsNullOrEmpty(TenSP))
             {
-                string uniqueFileName = null;
-                if (model.ImageUpload != null && model.ImageUpload.Length > 0)
-                {
-                    string uploadsFolder = Path.Combine(_hostEnvironment.WebRootPath, "images");
-                    uniqueFileName = Guid.NewGuid().ToString() + "_" + model.ImageUpload.FileName;
-                    string filePath = Path.Combine(uploadsFolder, uniqueFileName);
-
-                    using (var fileStream = new FileStream(filePath, FileMode.Create))
-                    {
-                        await model.ImageUpload.CopyToAsync(fileStream);
-                    }
-                }
-
-                // Chuyển từ ViewModel sang Entity
-                var sanPham = new SanPham
-                {
-                    TenSP = model.TenSP,
-                    MoTa = model.MoTa,
-                    DonGia = model.DonGia,
-                    GiaGoc = model.GiaGoc,
-                    SoLuong = model.SoLuong,
-                    ImageURL = "/images/" + uniqueFileName,
-                    MaDanhMuc = model.MaDanhMuc,
-                    MaThuongHieu = model.MaThuongHieu,
-                    ChieuRong = model.ChieuRong,
-                    ChieuCao = model.ChieuCao,
-                    ChieuSau = model.ChieuSau,
-                    NgayTao = DateTime.Now
-                };
-
-                _context.SanPhams.Add(sanPham);
-                await _context.SaveChangesAsync();
-                return RedirectToAction("Index");
+                ModelState.AddModelError("TenSP", "Tên sản phẩm là bắt buộc.");
+                ViewBag.DanhMucList = new SelectList(_context.DanhMucs, "MaDanhMuc", "TenDanhMuc", MaDanhMuc);
+                ViewBag.ThuongHieuList = new SelectList(_context.ThuongHieus, "MaThuongHieu", "TenThuongHieu", MaThuongHieu);
+                return View("~/Adminboot/Admin/Views/Sanpham/Create.cshtml");
             }
 
-            // Load lại dropdown nếu ModelState không hợp lệ
-            ViewBag.DanhMucList = new SelectList(_context.DanhMucs.ToList(), "MaDanhMuc", "TenDanhMuc", model.MaDanhMuc);
-            ViewBag.ThuongHieuList = new SelectList(_context.ThuongHieus.ToList(), "MaThuongHieu", "TenThuongHieu", model.MaThuongHieu);
+            string imagePath = null;
 
-            return View(model);
+            if (Anh != null && Anh.Length > 0)
+            {
+                var uploadsDir = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/uploads");
+                if (!Directory.Exists(uploadsDir))
+                    Directory.CreateDirectory(uploadsDir);
+
+                var fileName = Path.GetFileName(Anh.FileName);
+                var filePath = Path.Combine(uploadsDir, fileName);
+
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await Anh.CopyToAsync(stream);
+                }
+
+                imagePath = "/uploads/" + fileName;
+            }
+            else if (!string.IsNullOrEmpty(ImageURL))
+            {
+                imagePath = ImageURL;
+            }
+            else
+            {
+                imagePath = "/images/no-image.png";
+            }
+
+            var sanPham = new SanPham
+            {
+                TenSP = TenSP,
+                MoTa = MoTa,
+                DonGia = DonGia,
+                GiaGoc = GiaGoc,
+                SoLuong = SoLuong,
+                ChieuRong = (int?)ChieuRong,
+                ChieuCao = (int?)ChieuCao,
+                ChieuSau = (int?)ChieuSau,
+                MaDanhMuc = MaDanhMuc,
+                MaThuongHieu = MaThuongHieu,
+                ImageURL = imagePath,
+                NgayTao = DateTime.Now
+            };
+
+            _context.SanPhams.Add(sanPham);
+            await _context.SaveChangesAsync();
+
+            TempData["Success"] = "✅ Thêm sản phẩm thành công!";
+            return RedirectToAction("Index");
         }
-
     }
 }
