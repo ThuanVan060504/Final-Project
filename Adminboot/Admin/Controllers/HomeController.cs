6865679af0cbb7f5fe98f8d3e61a87a7022fe225
ThuanIT <thugoodcat@gmail.com>
2025-08-09 21:23:16 +0700

    Dashboard ad

--- Adminboot/Admin/Controllers/HomeController.cs
+++ Adminboot/Admin/Controllers/HomeController.cs
@@ -1,15 +1,75 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Final_Project.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
 
-namespace Final_Project.Adminboot.Admin.Controllers
+namespace Final_Project.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly string _connectionString;
+
+        public HomeController(IConfiguration configuration)
         {
-            return View("~/Adminboot/Admin/Views/Home/Index.cshtml");
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
         }
 
+        public IActionResult Index()
+        {
+            var model = new DashboardViewModel
+            {
+                TotalCustomers = 0,
+                TotalSuppliers = 0,
+                TotalSaleAmount = 0,
+                TotalSalesInvoice = 0,
+                RecentProducts = new List<ProductViewModel>()
+            };
 
+            using (var con = new SqlConnection(_connectionString))
+            {
+                con.Open();
+
+                // Customers
+                using (var cmd = new SqlCommand("SELECT COUNT(*) FROM TaiKhoan WHERE VaiTro = 'Khách hàng'", con))
+                    model.TotalCustomers = (int)cmd.ExecuteScalar();
+
+                // Suppliers
+                using (var cmd = new SqlCommand("SELECT COUNT(*) FROM NhaCungCap", con))
+                    model.TotalSuppliers = (int)cmd.ExecuteScalar();
+
+                // Total Sale Amount
+                using (var cmd = new SqlCommand("SELECT ISNULL(SUM(TongTien),0) FROM HoaDon", con))
+                    model.TotalSaleAmount = Convert.ToDecimal(cmd.ExecuteScalar());
+
+                // Sales Invoice Count
+                using (var cmd = new SqlCommand("SELECT COUNT(*) FROM HoaDon", con))
+                    model.TotalSalesInvoice = (int)cmd.ExecuteScalar();
+
+                // Recently Added Products
+                using (var cmd = new SqlCommand(@"
+            SELECT TOP 5 MaSP, TenSP, DonGia, ImageURL
+            FROM SanPham
+            ORDER BY NgayTao DESC", con))
+                {
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            model.RecentProducts.Add(new ProductViewModel
+                            {
+                                MaSP = reader.GetInt32(0),
+                                TenSP = reader.GetString(1),
+                                DonGia = reader.GetDecimal(2),
+                                ImageURL = reader.IsDBNull(3) ? "" : reader.GetString(3)
+                            });
+                        }
+                    }
+                }
+            }
+
+            return View("~/Adminboot/Admin/Views/Home/Index.cshtml", model);
+        }
     }
 }
---
+++ Models/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Final_Project.Models.ViewModels
+{
+    public class DashboardViewModel
+    {
+        public int TotalCustomers { get; set; }
+        public int TotalSuppliers { get; set; }
+        public decimal TotalSaleAmount { get; set; }
+        public int TotalSalesInvoice { get; set; }
+        public List<ProductViewModel> RecentProducts { get; set; } = new();
+    }
+
+    public class ProductViewModel
+    {
+        public int MaSP { get; set; }
+        public string TenSP { get; set; }
+        public decimal DonGia { get; set; }
+        public string ImageURL { get; set; }
+    }
+}
